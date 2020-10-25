5e9b44c07209c770687b571fb3467b854f634bdb
lpagn <lpagni@itba.edu.ar>
2020-10-25 16:16:12 -0300

    fix collator q5 problem

--- api/src/main/java/collators/collatorq5.java
+++ api/src/main/java/collators/collatorq5.java
@@ -16,7 +16,7 @@ public class collatorq5 implements Collator<Map.Entry<String, Integer>, List<Q5a
         List<Q5ans> ret = new LinkedList<>();
         for (Map.Entry<String, Integer> entryA : values) {
             for(Map.Entry<String, Integer> entryB : values){
-                if(entryA.getValue().equals(entryB.getValue()) && !entryA.getKey().equals(entryB.getKey())){
+                if(entryA.getValue().equals(entryB.getValue()) && entryA.getKey().compareTo(entryB.getKey())<0 && !entryA.getKey().equals(entryB.getKey())){
                     Q5ans q5ans = new Q5ans(entryA.getValue(),entryA.getKey(),entryB.getKey());
                     ret.add(q5ans);
                 }
