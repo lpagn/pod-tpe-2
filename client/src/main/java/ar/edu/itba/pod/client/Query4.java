439d3130dc99d153e24de0807f00443432deeca7
Juan <juanmartinbaader@hotmail.com>
2020-10-25 20:22:18 -0300

    fileWriters

--- client/src/main/java/ar/edu/itba/pod/client/Query4.java
+++ client/src/main/java/ar/edu/itba/pod/client/Query4.java
@@ -14,10 +14,17 @@ import combiners.CombinerFactory1Q4;
 import mappers.Mapper1Q4;
 import models.Pair;
 import models.Tree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import predicate.Predicate1Q4;
 import reducers.Reducer1Q4;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,60 +32,81 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class Query4 {
-    public static void main(String [] args) throws ExecutionException, InterruptedException {
+
+    private static final Logger logger = LoggerFactory.getLogger(Query1.class);
+
+    public static void main(String [] args) throws ExecutionException, InterruptedException, IOException {
         System.out.println("Query 4");
-        String city = System.getProperty("city");
+        final String city = System.getProperty("city");
         final String addresses = System.getProperty("addresses");
         final String inPath = System.getProperty("inPath");
         final String outPath = System.getProperty("outPath");
-        final String n = System.getProperty("n");
-
+        final String name = System.getProperty("name");
+        final int min= Integer.parseInt(System.getProperty("min"));
         final ClientConfig ccfg = new ClientConfig()
                 .setGroupConfig(new GroupConfig()
                         .setName("g10")
                         .setPassword("g10"));
-        int min=11000;
-        city="BUE";
-        String specie ="Fraxinus pennsylvanica";
-
         final HazelcastInstance client = HazelcastClient.newHazelcastClient(ccfg);
-
-        String s = QueryUtils.now() + " INFO [main] Query2 (Query2.java:xx) - Inicio de la lectura del archivo\n";
-        System.out.println(s);
 
         System.out.println(city + " " + addresses+ " " + inPath + " " + outPath + " " + min);
 
-        final IMap<Pair<Integer,String>, String> map3 = client.getMap("g10Q1NeighToTreeName");
-        map3.clear();
+        Files.deleteIfExists(Paths.get(outPath+"/timeStamps.csv"));
+        Files.deleteIfExists(Paths.get(outPath+"/result.csv"));
+
+        FileWriter timeStampWriter = new FileWriter(new File(outPath+"/timeStamps.txt"));
+        FileWriter csvWriter = new FileWriter(new File(outPath+"/result.csv"));
+
+        String s = QueryUtils.now() + " INFO [main] Query4 (Query4.java:xx) - Inicio de la lectura del archivo\n";
+        timeStampWriter.append(s);
+
+        final IMap<Pair<Integer,String>, String> map = client.getMap("g10Q1NeighToTreeName");
+        map.clear();
         URL arboles = Query1.class.getClassLoader().getResource("arbolesBUE.csv");
-        map3.putAll(Loader.loadNeighToTreeName(arboles.getFile(),city));
+        if(arboles == null){
+            logger.error("Error loading tree file");
+            System.exit(-1);
+        }
+        map.putAll(Loader.loadNeighToTreeName(arboles.getFile(),city));
 
-        String t = QueryUtils.now() + " INFO [main] Query2 (Query2.java:xx) - Fin de la lectura del archivo\n";
-        System.out.println(t);
 
-        String u = QueryUtils.now() + " INFO [main] Query2 (Query2.java:xx) - Inicio del trabajo map/reduce\n";
-        System.out.println(u);
+
+        String t = QueryUtils.now() + " INFO [main] Query4 (Query4.java:xx) - Fin de la lectura del archivo\n";
+        timeStampWriter.append(t);
+
+        String u = QueryUtils.now() + " INFO [main] Query4 (Query4.java:xx) - Inicio del trabajo map/reduce\n";
+        timeStampWriter.append(u);
 
         JobTracker jobTracker = client.getJobTracker("g10q4");
-        final KeyValueSource<Pair<Integer,String>, String> source = KeyValueSource.fromMap(map3);
+        final KeyValueSource<Pair<Integer,String>, String> source = KeyValueSource.fromMap(map);
         Job<Pair<Integer,String>, String> job = jobTracker.newJob(source);
         ICompletableFuture<List<Pair<String,String>>> future = job
-                .keyPredicate(new Predicate1Q4(specie))
+                .keyPredicate(new Predicate1Q4(name))
                 .mapper( new Mapper1Q4())
                 .combiner( new CombinerFactory1Q4() )
                 .reducer( new Reducer1Q4())
                 .submit( new Collator1Q4(min));
         // Attach a callback listener
 
-        String v = QueryUtils.now() + " INFO [main] Query2 (Query2.java:xx) - Fin del trabajo map/reduce\n";
-        System.out.println(v);
 
         List<Pair<String,String>> result = future.get();
 
-        // Wait and retrieve the result
-        System.out.println("Barrio A;Barrio B");
-        result.forEach((element) -> System.out.println(element.getKey() + ";" + element.getValue()));
+        String v = QueryUtils.now() + " INFO [main] Query4 (Query4.java:xx) - Fin del trabajo map/reduce\n";
+        timeStampWriter.append(v);
 
+        // Wait and retrieve the result
+        csvWriter.append("Barrio A;Barrio B\n");
+//        System.out.println("Barrio A;Barrio B");
+        result.forEach((element) -> {
+            try {
+                csvWriter.append(element.getKey() + ";" + element.getValue()+"\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        csvWriter.close();
+        timeStampWriter.close();
+        System.exit(0);
 //        JobTracker jobTracker = client.getJobTracker("g10q4");
 //        final KeyValueSource<Integer, Tree> source = KeyValueSource.fromMap(map2);
 //        Job<Integer, Tree> job = jobTracker.newJob(source);
