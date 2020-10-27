a88c5921c891e9f03840a0a823d1091043920b46
Juan <juanmartinbaader@hotmail.com>
2020-10-27 17:52:14 -0300

    query5 combiner

--- api/src/main/java/collators/CollatorQ5.java
+++ api/src/main/java/collators/CollatorQ5.java
@@ -17,7 +17,7 @@ public class CollatorQ5 implements Collator<Map.Entry<String, Integer>, List<Q5a
         for (Map.Entry<String, Integer> entryA : values) {
             for(Map.Entry<String, Integer> entryB : values){
                 if(entryA.getValue().equals(entryB.getValue()) && entryA.getKey().compareTo(entryB.getKey())<0 && !entryA.getKey().equals(entryB.getKey())){
-                    Q5ans q5ans = new Q5ans(entryA.getValue(),entryA.getKey(),entryB.getKey());
+                    Q5ans q5ans = new Q5ans(entryA.getValue()*1000,entryA.getKey(),entryB.getKey());
                     list.add(q5ans);
                 }
             }
