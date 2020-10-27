f1847d1a868a159ecac8f9a9237f61a7b09d7844
lpagn <lpagni@itba.edu.ar>
2020-10-27 10:11:28 -0300

    query3 printing

--- client/src/main/java/ar/edu/itba/pod/client/Query3.java
+++ client/src/main/java/ar/edu/itba/pod/client/Query3.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.pod.client;
 
 import ar.edu.itba.pod.client.utils.Loader;
+import ar.edu.itba.pod.client.utils.QueryUtils;
 import collators.collatorq3;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
@@ -16,21 +17,36 @@ import models.Pair;
 import models.Tree;
 import reducers.reducerq3;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class Query3 {
-    public static void main(String [] args) throws ExecutionException, InterruptedException{
+    public static void main(String [] args) throws ExecutionException, InterruptedException, IOException {
         System.out.println("Query 3");
         final String city = System.getProperty("city");
         final String addresses = System.getProperty("addresses");
         final String inPath = System.getProperty("inPath");
-        final String outPath = System.getProperty("outPath");
-        final String n = System.getProperty("n");
+        /*final*/ String outPath = System.getProperty("outPath");
+        /*final*/ String n = System.getProperty("n");
+
+        //Defaults
+        n = "1";
+        outPath = "/Users/luciopagni/Desktop/pod-tpe-2";
 
         System.out.println(city + " " + addresses+ " " + inPath + " " + outPath + " " + n);
+
+        Files.deleteIfExists(Paths.get(outPath+"/resultQuery3.csv"));
+        Files.deleteIfExists(Paths.get(outPath+"/timeStamps.csv"));
+
+        FileWriter csvWriter = new FileWriter(new File(outPath+"/resultQuery3.csv"));
+        FileWriter timeStampWriter = new FileWriter(new File(outPath+"/timeStamps.txt"));
 
         final ClientConfig ccfg = new ClientConfig()
                 .setGroupConfig(new GroupConfig()
@@ -42,34 +58,44 @@ public class Query3 {
         final IMap<Integer, Tree> map3 = client.getMap("g10Q3Trees");
         map3.clear();
         URL arboles = Query3.class.getClassLoader().getResource("arbolesBUE.csv");
+
+        String s = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Inicio de la lectura del archivo\n";
+        timeStampWriter.append(s);
+
         map3.putAll(Loader.loadTrees(arboles.getFile(),"BUE"));
 
+        String t = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Fin de la lectura del archivo\n";
+        timeStampWriter.append(t);
+
+        String u = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Inicio del trabajo map/reduce\n";
+        timeStampWriter.append(u);
 
         Job<Integer, Tree> job = client.getJobTracker("g10jt").newJob(KeyValueSource.fromMap(map3));
         JobCompletableFuture<List<Pair<String, Double>>> future = job
                 .mapper( new mapperq3() )
                 .reducer( new reducerq3() )
-                .submit(new collatorq3(10));//TODO: parametrize
+                .submit(new collatorq3(Integer.parseInt(n)));
 
         while(!future.isDone());
+
+        String v = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Fin del trabajo map/reduce\n";
+        timeStampWriter.append(v);
 
         List<Pair<String, Double>> result = future.get();
         //System.out.println(result);
 
         result.forEach((x)->System.out.println(x.getKey() + " " + x.getValue()));
 
-        //result.forEach((key,value) -> System.out.println("Key = [" + key + "], Value = [" + value + "]"));
-
-//        String s = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Inicio de la lectura del archivo\n";
-//        System.out.println(s);
-//
-//        String t = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Fin de la lectura del archivo\n";
-//        System.out.println(t);
-//
-//        String u = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Inicio del trabajo map/reduce\n";
-//        System.out.println(u);
-//
-//        String v = QueryUtils.now() + " INFO [main] Query3 (Query3.java:xx) - Fin del trabajo map/reduce\n";
-//        System.out.println(v);
+        csvWriter.append("NOMBRE_CIENTIFICO;PROMEDIO_DIAMETRO\n");
+        result.forEach((element) -> {
+            try {
+                csvWriter.append(element.getKey() + ";" + element.getValue()+"\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        csvWriter.close();
+        timeStampWriter.close();
+        System.exit(0);
     }
 }
